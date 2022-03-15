c7219616fb3de76a796065ec100ca0cc3526dde9
syailaalfitriyani <93575152+syailaalfitriyani@users.noreply.github.com>
2022-03-15 11:08:21 +0700

    Update peta.vue

--- src/components/peta.vue
+++ src/components/peta.vue
@@ -1,9 +1,5 @@
 <template>
     <div>
-           <br> Singkup,
-            Purbaratu
-            Tasikmalaya 
-            <br>
         <img src="../assets/peta.png" width="100%">
     </div>
 </template>
